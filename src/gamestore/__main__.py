0a209f16ee8e0b7b8ee182f187a16fee40607aa6
Zsombor Király <kiralyzsombor17@gmail.com>
2024-12-20 18:13:47 +0100

    #5 crud dev beg

--- src/gamestore/__main__.py
+++ src/gamestore/__main__.py
@@ -1,5 +1,10 @@
-
 import logging
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import uvicorn
+
+from gamestore.game_router import router as gr
 
 logger = logging.getLogger(__name__)
 
@@ -10,5 +15,19 @@ __version__ = "0.1.0"
 def main():
      logger.info("Staring GameStore")
 
+     app = FastAPI(
+           title="GameStore",
+           version=__version__
+     )
+
+     app.include_router(gr)
+
+     @app.get(path="/")
+     def docs():
+         return RedirectResponse(url="/docs")
+
+     uvicorn.run(app, log_config=None)
+
+
 if __name__ == '__main__':
-    main()
+     main()
